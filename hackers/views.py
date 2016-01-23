57c0a62e75dbd4f5c929bcdbeba078ba8a80ba8d
ahsanMah <amahmood@cu83069xp.colgate.edu>
2016-01-23 16:03:32 -0500

    Set up views

--- hackers/views.py
+++ hackers/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import BlogPost, Events
+
 
 def index(request):
 	template = loader.get_template('index.html')
@@ -8,10 +10,13 @@ def index(request):
 
     
 def blog_detail(request, post_id):
-    response = "You are looking blog post %s."
-    return HttpResponse(response % post_id)
+	latest_post_list = BlogPost.objects[:5]
+	context = {'latest_blog_list':latest_post_list}
+	template = loader.get_template('blog.html')
+	return render(request, 'post.html', context)
     
 def event_detail(request, event_id):
-    response = "You are looking event post %s."
-    return HttpResponse(response % event_id)
-
+	latest_events_list = Events.objects[:5]
+	context = {'latest_events_list':latest_events_list}
+	template = loader.get_template('events.html')
+	return render(request, 'events.html', context)
